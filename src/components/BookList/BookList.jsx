f4202e01d76ac671a6c483862e7261d6ac055530
184744 <184744@ssg.com>
2021-03-16 15:23:57 +0900

    booklist수정

--- src/components/BookList/BookList.jsx
+++ src/components/BookList/BookList.jsx
@@ -18,6 +18,10 @@ const BookList = ({ authService }) => {
 		});
 	}, []);
 
+	const onCheck = (e) => {
+		e.target.closest("tr").classList.toggle(styles.check);
+	}
+
 	return (
 		<section className={styles.booklist}>
 			{emailName !==null && (
@@ -26,13 +30,36 @@ const BookList = ({ authService }) => {
 				</h1>
 			)}
 			<div className={styles.bookBox}>
-				<ul className={styles.bookList}>
-					<li>자바스크립트 개론</li>
-				</ul>
+				<table className={styles.bookListTbl}>
+					<thead>
+						<tr>
+							<th className={styles.checked}></th>
+							<th className={styles.bookName}>책이름</th>
+							<th className={styles.publisher}>출판사</th>
+							<th className={styles.date}>발행일</th>
+							<th className={styles.status}>대여여부</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td><input className={styles.checkbox} type="checkbox" name="check" onClick={onCheck} /></td>
+							<td>자바스크립트 개론</td>
+							<td>금성출판사</td>
+							<td>2021.03.16</td>
+							<td>육선도 <span className={styles.stateBook}>(대여중)</span></td>
+						</tr>
+					</tbody>
+				</table>
 			</div>
 			<div className={styles.buttonBox}>
+				<button type="button" className={styles.rental}>
+					빌려가기
+				</button>
 				<button type="button" className={styles.addBook}>
 					도서추가
+				</button>
+				<button type="button" className={styles.removeBook}>
+					도서삭제
 				</button>
 				<button type="button" className={styles.logoutBox} onClick={onLogout}>
 					로그아웃
