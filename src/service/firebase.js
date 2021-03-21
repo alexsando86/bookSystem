cefd0d3b3f0b15eae2a2bd4ad18ae8bfd28ae2f2
alexsando86 <yukseondo@naver.com>
2021-03-21 21:21:53 +0900

    'database'

--- src/service/firebase.js
+++ src/service/firebase.js
@@ -4,6 +4,8 @@ const firebaseConfig = {
 	apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
 	authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
 	projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+	databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+	storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
 };
 
 // Initialize Firebase
